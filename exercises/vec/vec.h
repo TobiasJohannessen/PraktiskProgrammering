9961a264491d82773a0a5f84ae566145aae43215
PrakProg <tobias-bach@live.dk>
2025-02-24 21:29:22 +0100

    Vec exercise final touches

--- exercises/vec/vec.h
+++ exercises/vec/vec.h
@@ -2,7 +2,6 @@
 #define HAVE_VEC_H
 #include<iostream>
 #include<string>
-#include<cmath>
 
 struct vec{
     double x,y,z;
@@ -13,32 +12,25 @@ struct vec{
     ~vec()=default; // destructor
     vec& operator=(const vec&)=default; // copy assignment
     vec& operator=(vec&&)=default; // move assignment
-    vec& operator+=(const vec& v){x+=v.x; y+=v.y; z+=v.z; return *this;};
-    vec& operator-=(const vec& v){x-=v.x; y-=v.y; z-=v.z; return *this;};
-    vec& operator*=(double c){x*=c; y*=c; z*=c; return *this;};;
-    vec& operator/=(double c){x/=c; y/=c; z/=c; return *this;};;
+    vec& operator+=(const vec&);
+    vec& operator-=(const vec&);
+    vec& operator*=(double);
+    vec& operator/=(double c);
     void set(double a,double b,double c){x=a;y=b;z=c;}
-    void print(std::string s="") const{
-        std::cout << s << x << " " << y << " " << z << std::endl; // for debugging
-    }; 
+    void print(std::string s="") const; // for debugging
+    double dot(const vec& other);
+    vec cross(const vec& other) const;
     friend std::ostream& operator<<(std::ostream&, const vec&);
 };
-vec operator-(const vec& v){return vec(-v.x, -v.y, -v.z);};
-vec operator-(const vec& v1, const vec& v2){return vec(v1.x -v2.x, v1.y - v2.y, v1.z - v2.z);};
-vec operator+(const vec& v1, const vec& v2){return vec(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);};
-vec operator*(const vec& v, double c){return vec(v.x*c, v.y*c, v.z*c);};
-vec operator*(double c, const vec& v){return v*c;};
-vec operator/(const vec& v, double c){return vec(v.x/c, v.y/c, v.z/c);};
-bool approx(double a, double b, double acc=1e-9, double eps=1e-9){
-    double diff = abs(a-b);
-    double size = std::max(abs(a), abs(b));
-    if (diff <= acc){
-        return true;
-    };
-    if (diff/size <= eps){
-        return true;
-    };
-    return false;
+vec operator-(const vec& v);
+vec operator-(const vec& v1, const vec& v2);
+vec operator+(const vec& v1, const vec& v2);
+vec operator*(const vec& v, double c);
+vec operator*(double c, const vec& v);
+vec operator/(const vec& v, double c);
 
-};
+double dot(vec& v, vec& w);
+vec cross(const vec& v, const vec& w);
+bool approx(double a, double b, double acc=1e-9, double eps=1e-9);
+bool approx(const vec& u, const vec& v, double acc=1e-9, double eps=1e-9);
 #endif
